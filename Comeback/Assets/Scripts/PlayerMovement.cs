0f0ff1092a0ca2e3141ea8769c29b2323f250f85
Mycal <mycal.latchman@outlook.com>
2025-08-04 16:54:17 +0100

    Added jumping.
    Has cooldown, but it resets when touching the ground.

--- Comeback/Assets/Scripts/PlayerMovement.cs
+++ Comeback/Assets/Scripts/PlayerMovement.cs
@@ -1,14 +1,30 @@
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
+    private Rigidbody2D player;
+
     public float moveHoz;
     public float moveSpeed;
-    private Rigidbody2D player;
+    public float speedLimit;
+
+    public float moveVert;
+    public float jumpForce;
+    public bool canJump;
+    public float jumpCooldown;
+    public float jumpTimer;
+    public bool grounded;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
         player = gameObject.GetComponent<Rigidbody2D>();
+        moveHoz = 0f;
         moveSpeed = 1f;
+        speedLimit = 7f;
+        jumpForce = 50f;
+        canJump = false;
+        jumpCooldown = 6f;
+        jumpTimer = 0.0f;
+        grounded = false;
     }
 
     // Update is called once per frame
@@ -25,11 +41,54 @@ public class PlayerMovement : MonoBehaviour {
         else if (Input.GetKeyUp(KeyCode.A)) {
             moveHoz = 0;
         }
+
+        if (Input.GetKeyDown(KeyCode.W)) {
+            moveVert = 1;
+        }
+        else if (Input.GetKeyUp(KeyCode.W)) {
+            moveVert = 0;
+        }
     }
 
     private void FixedUpdate() {
+        //Horizonal movement
         if (moveHoz > 0.1f || moveHoz < -0.1f) {
             player.AddForce(new Vector2(moveHoz * moveSpeed, 0f), ForceMode2D.Impulse);
+        }
+        //Jump cooldown
+        if (jumpTimer >= jumpCooldown) {
+            canJump = true;
+        }
+        else {
+            jumpTimer += Time.deltaTime;
+            canJump = false;
+        }
+        //Jump  
+        if (canJump && moveVert > 0.1f) {
+            player.AddForce(new Vector2(0f, moveVert * jumpForce), ForceMode2D.Impulse);
+            jumpTimer = 0.0f;
+            grounded = false;
+        }
+        //Speed limit
+        if (moveSpeed > speedLimit) {
+            moveHoz = speedLimit;
+        }
+        if (grounded) {
+            jumpTimer = jumpCooldown;
+        }
+    }
+
+    //Checks if game object is in contact with the ground, if so it is not jumping. (Using "Surface" tag)
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.tag == "Surface") {
+            grounded = true;
+        }
+    }
+
+    //Check if game object is in contact with the ground, if not it is jumping.
+    private void OnCollisionExit2D(Collision2D collision) {
+        if (collision.gameObject.tag == "Surface") {
+            grounded = false;
         }
     }
 }
